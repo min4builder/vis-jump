1bc6609f349b6530490ee6a6e22846f4fec01245
Matěj Cepl <mcepl@cepl.eu>
2020-03-16 23:18:06 +0100

    Fix calculating to; reformat

--- init.lua
+++ init.lua
@@ -4,15 +4,10 @@ local function get_query()
     local str = vis.win.file.lines[line]
     local len_str = string.len(str)
 
-    local to = str:find('%s', pos) - 1
-    if to == nil then to = string.len(str) end
+    local to = str:find('%s', pos)
+    if to == nil then to = len_str else to = to - 1 end
     local from = str:reverse():find('%s', len_str - pos + 1)
-    if from == nil then
-        from = 1
-    else
-        -- reverse index
-        from = len_str - from + 2
-    end
+    if from == nil then from = 1 else from = len_str - from + 2 end
     return str:sub(from, to)
 end
 
@@ -20,5 +15,6 @@ vis:map(vis.modes.NORMAL, "gx", function()
     local cur_word = get_query()
     -- https://bugs.freedesktop.org/show_bug.cgi?id=103807  x
     -- https://www.damejidlo.cz/potrefena-husa-vinohrady
-    os.execute("xdg-open '" .. cur_word .. "'")
+    -- gh#python/cpython#7778 or bpo#34032
+    os.execute("osurl '" .. cur_word .. "'")
 end, "Jump to URL")
